b9f68b94935d15817104e95786319326b11cc59d
prabodhw96 <prabodhwankhede1996@gmail.com>
2018-12-25 18:16:33 +0530

    Minor change

--- gtzan.py
+++ gtzan.py
@@ -10,13 +10,11 @@ from keras.models import load_model
 # Disable TF warnings about speed up and future warnings
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-# Constants
 song_samples = 660000
 genres = {'metal': 0, 'disco': 1, 'classical': 2, 'hiphop': 3, 'jazz': 4, 
           'country': 5, 'pop': 6, 'blues': 7, 'reggae': 8, 'rock': 9}
 
 def splitsongs(X, window = 0.1, overlap = 0.5):
-    # Empty lists to hold our results
     temp_X = []
     # Get the input song array size
     xshape = X.shape[0]
@@ -36,6 +34,7 @@ def to_melspectrogram(songs, n_fft = 1024, hop_length = 512):
     tsongs = map(melspec, songs)
     return np.array(list(tsongs))
 
+# Helper function to get key by value from a dictionary
 def getKeysByValue(dictOfElements, valueToFind):
     listOfKeys = list()
     listOfItems = dictOfElements.items()
@@ -71,6 +70,5 @@ if __name__ == '__main__':
     parser.add_argument('-m', '--model', help='If test is chosen, path to trained model', type=str)
     parser.add_argument('-s', '--song', help='If test is chosen, path to song to classify', type=str)
     args = parser.parse_args()
-    
     # Call the main function
     main(args)
